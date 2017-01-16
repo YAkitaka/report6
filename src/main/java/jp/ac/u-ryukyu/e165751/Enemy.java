471c2652a9c9d97b2e41ebbe18b8d39ca93f8033
Akitaka Yamashita <e165751@ie.uryukyu.ac.jp>
2017-01-17 07:14:05 +0900

    report6

--- src/main/java/jp/ac/u-ryukyu/e165751/Enemy.java
+++ src/main/java/jp/ac/u-ryukyu/e165751/Enemy.java
@@ -1,5 +1,6 @@
 package jp.ac.uryukyu.ie.e165751;
 
+
 /**
  * Created by e165751 on 2016/11/18.
  */
@@ -13,6 +14,23 @@ public class Enemy extends LivingThing{
         if (getHitPoint() < 0) {
             setDead();
             System.out.printf("モンスター%sは倒れた。\n", getName());
+        }
+    }
+    @Override
+    public void attack(LivingThing opponent) {
+        if (!isDead()) {
+            int damage = (int) (Math.random() * getAttack());
+            if (damage == 0) {
+                System.out.printf("%sの攻撃！,,,だが、%sは攻撃を回避！！\n", getName(), opponent.getName());
+            } else if (Math.random() < 0.2) {
+                System.out.printf("%sの攻撃！痛恨の一撃！！%sに％dのダメージ与えた\n", getName(), opponent.getName(), damage * 2);
+                opponent.wounded(damage * 2);
+
+            }else{
+                System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", getName(), opponent.getName(), damage);
+                opponent.wounded(damage);
+
+            }
         }
     }
 }
--- src/main/java/jp/ac/u-ryukyu/e165751/Hero.java
+++ src/main/java/jp/ac/u-ryukyu/e165751/Hero.java
@@ -17,4 +17,29 @@ public class Hero extends LivingThing{
             System.out.printf("勇者%sは道半ばで力尽きてしまった。\n", getName());
         }
     }
+
+
+   @Override
+    public void attack(LivingThing opponent) {
+       if (!isDead()) {
+           int damage = (int) (Math.random() * getAttack());
+           if (damage == 0) {
+               System.out.printf("%sの攻撃！,,,だが、%sは攻撃を回避！！\n", getName(), opponent.getName());
+           } else if (Math.random() < 0.3) {
+                   System.out.printf("%sの攻撃！痛恨の一撃！！%sに%dのダメージ与えた\n", getName(), opponent.getName(), damage * 2);
+                   opponent.wounded(damage * 2);
+
+           }else{
+               System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", getName(), opponent.getName(), damage);
+               opponent.wounded(damage);
+
+           }
+       }
+   }
 }
+
+
+
+
+
+
